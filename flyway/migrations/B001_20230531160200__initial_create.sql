2feca00390e7df28fe48e6da9ed25489bce511c5
Alex Yates <Alex.Yates@red-gate.com>
2023-05-31 16:21:56 +0100

    initial commit

---
+++ flyway/migrations/B001_20230531160200__initial_create.sql
@@ -0,0 +1,21 @@
+/****** Object:  Table [dbo].[hello]    Script Date: 2/13/2023 10:42:07 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[hello](
+	[foo] [nchar](10) NULL,
+	[bar] [nchar](10) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[world]    Script Date: 2/13/2023 10:42:07 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[world](
+	[foo] [nchar](10) NULL,
+	[bar] [nchar](10) NULL,
+	[baz] [nchar](10) NULL
+) ON [PRIMARY]
+GO
---
+++ flyway/migrations/V002_20230531170200__Create_foobar.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[foobar](
+	[foo] [nchar](10) NULL,
+	[bar] [nchar](10) NULL
+) ON [PRIMARY]
+GO
