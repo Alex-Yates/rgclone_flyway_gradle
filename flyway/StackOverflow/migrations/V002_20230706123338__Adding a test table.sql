178ce7aff43dfc33a2008a97cc4e7bbdf6fe0ef1
Barry Attwater <barry.attwater@red-gate.com>
2023-07-14 13:38:53 +0100

    Tidy up

--- flyway/StackOverflow/migrations/V002_20230706123338__Adding a test table.sql
+++
@@ -1,9 +0,0 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[test_table](
-	[foo] [nchar](10) NULL,
-	[bar] [nchar](10) NULL
-) ON [PRIMARY]
-GO
--- flyway/StackOverflow/schema-model/Tables/dbo.test_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[test_table]
-(
-[foo] [nchar] (10) NULL,
-[bar] [nchar] (10) NULL
-)
-GO
